5f44fe5f5e0f8cc1fbffdaaa08bcc73711a8a157
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-10-03 00:43:56 +0300

    update scroll and preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,29 @@
-// selectElement = document.querySelector("#GRADE");
-// var oInputs = new Array();
-// oInputs = document.getElementsByTagName("input");
+/*******************************************************
+# back top the top
+**********************************************************/
+const back_to_top = document.querySelector(".back_to_top");
 
-// var sum = 0;
-// var res = 0;
-// var n = 0;
-// var nOfHours = 0;
-// function GPA() {
-//   for (i = 0; i < oInputs.length; i++) {
-//     if (oInputs[i].type == "number") {
-//       n = oInputs[i].value;
-//     }
-//   }
-//   output = selectElement.options[selectElement.selectedIndex].value;
-//   nOfHours = nOfHours + n;
-//   res = res + n * output;
-// }
-// function ADD() {
-//   var course = document.getElementById("CourseName").value;
-//   var degree = document.getElementById("CourseHour").value;
-//   output = selectElement.options[selectElement.selectedIndex].value;
-//   var table = document.getElementsByTagName("table")[0];
-//   var newRow = table.insertRow(table.rows.length);
-//   var cell0 = newRow.insertCell(0);
-//   var cell2 = newRow.insertCell(1);
-//   var cell3 = newRow.insertCell(2);
-//   cell0.innerHTML = course;
-//   cell2.innerHTML = degree;
-//   cell3.innerHTML = output;
-//   GPA();
-// }
-// function Calc() {
-//   var x = document.getElementById("prosses");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-
-//   while (nOfHours != 0) {
-//     sum = sum + (nOfHours % 10);
-//     nOfHours = parseInt(nOfHours / 10);
-//   }
-//   res = res / sum;
-//   document.querySelector(".output").textContent = res;
-//   reset();
-// }
-// function reset() {
-//   sum = 0;
-//   res = 0;
-//   n = 0;
-//   nOfHours = 0;
-// }
-
+window.onscroll = () => {
+  if (window.pageYOffset > 150) {
+    back_to_top.classList.add("active");
+  } else {
+    back_to_top.classList.remove("active");
+  }
+};
+back_to_top.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+/*******************************************************
+# preloader
+**********************************************************/
+const preloader = document.querySelector(".preloader");
+window.onload = () => {
+  preloader.classList.add("active");
+};
 /*******************************************************
 # Navmenu
 **********************************************************/
